ec2fc35a406bc93e019d8ff00535a816334249f8
ttbinh139 <ttbinh139@gmail.com>
2022-06-28 04:30:45 +0000

    Exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+end
+
+store3 = Store.third
+
+store3.delete
+
+puts "Total stores: #{Store.all.length}"
