ccee3dd1185ed1bed79c9ddcd7fece4efac1ba3f
ttbinh139 <ttbinh139@gmail.com>
2022-06-28 04:50:20 +0000

    Exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+end
+
+puts "Total revenue: #{Store.sum(:annual_revenue)}"
+puts "Average revenue: #{Store.average(:annual_revenue)}"
+
+good_stores = Store.where('annual_revenue > ?', 1000000)
+puts "Number of stores that are generating $1M or more: #{good_stores.length}"
